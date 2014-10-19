adce9c116e8830f832b0229f101223f8295db492
Emiliano <emi@Asterix.local>
2014-10-19 01:47:53 -0300

    angular comm test

--- TubeCurator-web/src/main/webapp/js/app.js
+++ TubeCurator-web/src/main/webapp/js/app.js
@@ -1,3 +1,12 @@
 (function() {
 	var app = angular.module('tubeCurator', []);
+	app.controller('TubeController', ['$http', function($http) {
+		var ctrl = this;
+		this.tubeEntries = [];
+		$http.get('http://localhost:18080/tubecurator/rest/resttest/tito?pregunta=caca').success(function(data) {
+			ctrl.tubeEntries = data; 
+		});
+
+	}]);
+	
 })();
