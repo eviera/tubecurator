8d09f14e24ecf929d35aaa92694534cb32eff1af
eviera@sdd <eviera@sdd>
2014-11-10 16:37:20 -0300

    TUB-003 al clickear sobre una entrada, navega a una vista de detalle

--- TubeCurator-web/src/main/java/net/eviera/tubecurator/TubeRest.java
+++ TubeCurator-web/src/main/java/net/eviera/tubecurator/TubeRest.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.ejb.EJB;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import net.eviera.tubecurator.entity.TubeEntry;
@@ -23,4 +24,11 @@ public class TubeRest {
 		return result;
 	}
 
+	@GET
+	@Path("{id}")
+	@Produces("application/json")
+	public TubeEntry getById(@PathParam("id") Long id) {
+		TubeEntry result = tubeEntryManager.getById(id);
+		return result;
+	}
 }
