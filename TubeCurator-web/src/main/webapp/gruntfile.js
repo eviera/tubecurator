666a924c260e60f2235a3e07e2849c566b2f9267
Emiliano <emi@Asterix.local>
2014-11-22 21:39:55 -0300

    grunt y minified app.js

--- TubeCurator-web/src/main/webapp/gruntfile.js
+++ TubeCurator-web/src/main/webapp/gruntfile.js
@@ -2,14 +2,25 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+  	pkg: grunt.file.readJSON('package.json'),
     jshint: {
-      all:['js/app.js']
-    }
+		all:['js/app.js']
+    },
+    uglify: {
+    	options: {
+        	banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+    	},
+		build: {
+			src: 'js/app.js',
+			dest: 'js/app.min.js'
+		}
+	}
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   // Default task(s).
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint','uglify']);
 
 };
